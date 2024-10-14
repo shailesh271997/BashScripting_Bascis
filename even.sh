24951f1ad2b748cec5fc42c6a816200342053d80
Ubuntu <ubuntu@ip-172-31-42-138.ec2.internal>
2024-10-14 07:11:43 +0000

    Check even number script added

---
+++ even.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+<<note 
+This is a script to check the number 
+even or odd
+note
+
+function check_even(){
+
+if [ $(( $1 % 2 )) -eq 0 ]; 
+then
+	echo "The number $1 is even"
+else 
+	echo "The number $1 is odd"
+fi
+
+}
+
+read -p "Enter your number: " number
+check_even $number
+
